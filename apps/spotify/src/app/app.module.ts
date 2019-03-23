a593c84e2d064ec6ded93a059e58567f2ac1d9c6
Victor Avila <victor.avila@venturplex.com>
2019-03-22 17:28:31 -0700

    add: angular material to app

--- apps/spotify/src/app/app.module.ts
+++ apps/spotify/src/app/app.module.ts
@@ -1,12 +1,20 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+import { MaterialModule } from '@cors/material';
+
 import { AppComponent } from './app.component';
 import { LoginComponent } from './login/login.component';
 
 @NgModule({
-  declarations: [AppComponent, LoginComponent],
-  imports: [BrowserModule],
+  declarations: [
+    AppComponent,
+    LoginComponent
+  ],
+  imports: [
+    BrowserModule,
+    MaterialModule
+  ],
   providers: [],
   bootstrap: [AppComponent]
 })
